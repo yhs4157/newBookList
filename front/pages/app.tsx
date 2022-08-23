a972505a98c51bc0136fe6d8703f5506cdd2fe14
yhs4157 <yhs4157@naver.com>
2022-08-23 16:35:44 +0900

    test

--- front/pages/app.tsx
+++ front/pages/app.tsx
@@ -4,13 +4,9 @@ const Homepage = () => {
     return (
         <div>
             <h1>Hello World!</h1>
-<<<<<<< HEAD
-            <h2>Test</h2>
-=======
             <h2>TEst</h2>
             <h3>TEst2</h3>
             <h4>Test3</h4>
->>>>>>> master
         </div>
     )
 }
