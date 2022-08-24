34202f082e62f5fd786dd8dfd21b4ccd781a054e
yhs4157 <yhs4157@naver.com>
2022-08-24 23:27:24 +0900

    test: Material UI

---
+++ front/components/Book/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import {
+Paper
+
+} from '@mui/material';
+
+const Book = () => {
+    return (
+        <div>
+            <Paper />    
+        </div>
+    )
+}
+
+export default Book; 
---
+++ front/components/BookList/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+Paper
+} from '@mui/material';
+
+const BookList = () => {
+    return (
+        <div>
+            <Paper />
+        </div>
+    )
+}
+
+export default BookList; 
